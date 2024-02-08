38084448b9fbb2145a77d5f72d95893b09d79502
zqzqian <S10261352@connect.np.edu.sg>
2024-02-08 16:15:29 +0800

    sign up and forum javascript

---
+++ script.js
@@ -0,0 +1,207 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+    const APIKEY = "65c47aedc5ab47165bcd60ba";
+    getContacts();
+    document.getElementById("update-userinfo-container").style.display = "none";
+    document.getElementById("add-update-msg").style.display = "none";
+  
+    document.getElementById("userinfo-submit").addEventListener("click", function (e) {
+
+      e.preventDefault();
+   
+      let infoUsername = document.getElementById("userinfo-username").value;
+      let infoEmail = document.getElementById("userinfo-email").value;
+      let infoPassword = document.getElementById("userinfo-password").value;
+
+      let jsondata = {
+        "username": infoUsername,
+        "email": infoEmail,
+        "password": infoPassword
+      };
+  
+      let settings = {
+        method: "POST", 
+        headers: {
+          "Content-Type": "application/json",
+          "x-apikey": APIKEY,
+          "Cache-Control": "no-cache"
+        },
+        body: JSON.stringify(jsondata),
+        beforeSend: function () {
+
+          document.getElementById("userinfo-submit").disabled = true;
+          document.getElementById("add-userinfo-form").reset();
+        }
+      }
+  
+      fetch("https://salvagedsavings-47eb.restdb.io/rest/userinfo", settings)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          document.getElementById("userinfo-submit").disabled = true;
+          document.getElementById("add-update-msg").style.display = "block";
+          setTimeout(function () {
+            document.getElementById("add-update-msg").style.display = "none";
+          }, 3000);
+
+          getContacts();
+        });
+    });
+  
+  
+    function getContacts(limit = 10, all = true) {
+  
+      let settings = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-apikey": APIKEY,
+          "Cache-Control": "no-cache"
+        },
+      }
+  
+      fetch("https://salvagedsavings-47eb.restdb.io/rest/userinfo", settings)
+      .then(response => response.json())
+      .then(response => {
+        let content = "";
+
+        for (var i = 0; i < response.length && i < limit; i++) {
+          content = `${content}<tr id='${response[i]._id}'>
+          <td>${response[i].username}</td>
+          <td>${response[i].email}</td>
+          <td>${response[i].password}</td>
+          <td><a href='#' class='delete' data-id='${response[i]._id}'>Del</a></td><td><a href='#update-userinfo-container' class='update' data-id='${response[i]._id}' data-username='${response[i].username}' data-email='${response[i].email}' data-password='${response[i].password}'>Update</a></td></tr>`;
+
+        }
+
+        document.getElementById("userinfo-list").getElementsByTagName("tbody")[0].innerHTML = content;
+
+        document.getElementById("total-userinfo").innerHTML = response.length;
+      });
+  }
+
+  document.getElementById("userinfo-list").addEventListener("click", function (e) {
+    if (e.target.classList.contains("update")) {
+      e.preventDefault();
+
+      let infoUsername = e.target.getAttribute("data-username");
+      let infoEmail = e.target.getAttribute("data-email");
+      let infoPassword = e.target.getAttribute("data-password");
+      let contactId = e.target.getAttribute("data-id");
+
+
+      document.getElementById("update-userinfo-username").value = infoUsername;
+      document.getElementById("update-userinfo-email").value = infoEmail;
+      document.getElementById("update-userinfo-password").value = infoPassword;
+      document.getElementById("update-userinfo-id").value = contactId;
+      document.getElementById("update-userinfo-container").style.display = "block";
+    }
+  });
+
+  document.getElementById("update-userinfo-submit").addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let infoUsername = document.getElementById("update-userinfo-username").value;
+    let infoEmail = document.getElementById("update-userinfo-email").value;
+    let infoPassword = document.getElementById("update-userinfo-password").value;
+    let contactId = document.getElementById("update-userinfo-id").value;
+
+    console.log(document.getElementById("update-userinfo-msg").value);
+    console.log(contactMsg);
+
+    updateForm(contactId, infoUsername, infoEmail, infoPassword, contactMsg);
+  });
+
+  function updateForm(id, infoUsername, infoEmail, infoPassword) { 
+
+    var jsondata = { "username": infoUsername, "email": infoEmail, "password": infoPassword };
+    var settings = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "x-apikey": APIKEY,
+        "Cache-Control": "no-cache"
+      },
+      body: JSON.stringify(jsondata)
+    }
+
+    fetch(`https://salvagedsavings-47eb.restdb.io/rest/userinfo/${id}`, settings)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        document.getElementById("update-userinfo-container").style.display = "none";
+
+        getContacts();
+      });
+  }
+
+  document.getElementById("userinfo-submit").addEventListener("click", function(e) {
+    e.preventDefault(); 
+
+    let infoUsername = document.getElementById("userinfo-username").value;
+    let infoEmail = document.getElementById("userinfo-email").value;
+    let infoPassword = document.getElementById("userinfo-password").value;
+
+    let jsondata = {
+      "username": infoUsername,
+      "email": infoEmail,
+      "password": infoPassword
+    };
+
+    let settings = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-apikey": APIKEY,
+        "Cache-Control": "no-cache"
+      },
+      body: JSON.stringify(jsondata),
+      beforeSend: function() {
+        document.getElementById("userinfo-submit").disabled = true;
+        document.getElementById("add-userinfo-form").reset();
+      }
+    };
+
+    fetch("https://salvagedsavings-47eb.restdb.io/rest/userinfo", settings)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        document.getElementById("userinfo-submit").disabled = false;
+
+        window.location.href = "lottie.html";
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        document.getElementById("userinfo-submit").disabled = false;
+      });
+  });
+
+});
+
+////////////////////////// forum //////////////////////////////////////////
+
+var likeCount = document.getElementsByClassName("likeCount");
+
+function likeButton(like, likeCount) {
+  var currentLikeCount = parseInt(likeCount.innerText);
+
+  if (like.classList.contains("fa-solid")) {
+    like.classList.remove("fa-solid");
+    like.classList.add("fa-regular");
+    currentLikeCount = currentLikeCount - 1;
+  } else {
+    like.classList.remove("fa-regular");
+    like.classList.add("fa-solid");
+    currentLikeCount = currentLikeCount + 1;
+  }
+
+  likeCount.innerText = currentLikeCount;
+}
+
+function on(overlayId) {
+  document.getElementById(overlayId).style.display = "block";
+}
+
+function off(overlayId) {
+  document.getElementById(overlayId).style.display = "none";
+}
